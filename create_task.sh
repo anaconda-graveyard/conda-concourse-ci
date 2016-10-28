27b37cb012ef15b5f69e6da4f8629bbd6305804e
Michael Sarahan <msarahan@gmail.com>
2016-10-28 11:40:48 -0500

    try more tasks

--- create_task.sh
+++ create_task.sh
@@ -5,27 +5,63 @@ pwd
 # debugging - this folder is automatically created when running with concourse
 mkdir -p ci-tasks
 
-cat << EOF > ci-tasks/generated.yml
+cat << EOF > ci-tasks/plan.yml
 ---
-resources:
-- name: execute-tasks
-  type: concourse-pipeline
+resource_types:
+# This allows us to upload/download multiple files to s3 at once
+- name: s3-simple
+  type: docker-image
   source:
-    teams:
-    - name: main
+    repository: 18fgsa/s3-resource-simple
+
+resources:
+- name: s3-intermediary
+  type: s3-simple
+  trigger: true
+  source:
+    bucket: {{aws-bucket}}
+    access_key_id: {{aws-key-id}}
+    secret_access_key: {{aws-secret-key}}
+    options:
+      - "--exclude '*'"
+      - "--include 'c*'"
 
 jobs:
-- name: set-execute-tasks
+- name: execute-tasks
   plan:
-  - put: execute-tasks
-    params:
-      pipelines:
-      - name: execute-task
-        team: main
-        config_file: ci-tasks/generated.yml
-        # vars_files:
-        # - path/to/optional/vars/file/1
-        # - path/to/optional/vars/file/2
+  - get: s3-intermediary
+  - aggregate:
+    - task: linux
+      file: s3-intermediary/ci-tasks/linux.yml
+    - task: windows
+      file: s3-intermediary/ci-tasks/win.yml
+EOF
+
+
+cat <<EOF > ci-tasks/linux.yml
+---
+platform: linux
+
+image_resource:
+  type: docker-image
+  source: {repository: busybox}
+
+run:
+  path: echo
+  args: [hello world from linux]
+EOF
+
+cat <<EOF > ci-tasks/win.yml
+---
+platform: linux
+
+image_resource:
+  type: docker-image
+  source: {repository: busybox}
+
+run:
+  path: echo
+  args: [hello world from win]
 EOF
 
 ls -la ci-tasks
