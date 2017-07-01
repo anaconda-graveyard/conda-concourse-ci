5035a427092f14d41afcdc4d73e2d37e67514dcb
Michael Sarahan <msarahan@gmail.com>
2017-07-01 18:39:11 -0500

    run docker as root

--- docker/install_miniconda.sh
+++ docker/install_miniconda.sh
@@ -1,12 +1,12 @@
 fname=$(find . -name Miniconda* -type f -exec ls -1t "{}" +; )
 # this is Ray Donnelly's custom Miniconda, built with our new gcc toolchain
-bash -x $fname -bfp /home/dev/miniconda
+bash -x $fname -bfp /opt/miniconda
 rm $fname
-/home/dev/miniconda/bin/conda config --set show_channel_urls True
-/home/dev/miniconda/bin/conda update --yes --all
-/home/dev/miniconda/bin/conda install --yes -c conda-canary git conda-build curl anaconda-client
-/home/dev/miniconda/bin/conda install --yes -c msarahan conda-concourse-ci
-/home/dev/miniconda/bin/conda config --add channels rdonnelly
-/home/dev/miniconda/bin/conda config --set add_pip_as_python_dependency False
-/home/dev/miniconda/bin/conda config --set anaconda_upload True
-/home/dev/miniconda/bin/conda clean -ptiy
+/opt/miniconda/bin/conda config --set show_channel_urls True
+/opt/miniconda/bin/conda update --yes --all
+/opt/miniconda/bin/conda install --yes -c conda-canary git conda-build curl anaconda-client
+/opt/miniconda/bin/conda install --yes -c msarahan conda-concourse-ci
+/opt/miniconda/bin/conda config --add channels rdonnelly
+/opt/miniconda/bin/conda config --set add_pip_as_python_dependency False
+/opt/miniconda/bin/conda config --set anaconda_upload True
+/opt/miniconda/bin/conda clean -ptiy
