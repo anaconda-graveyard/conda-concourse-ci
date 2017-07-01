e01d9314bb50cbcf8ff759b0b5bf6d6a6d9653fe
Michael Sarahan <msarahan@gmail.com>
2017-07-01 15:07:57 -0500

    tweak miniconda install script

--- docker/install_miniconda.sh
+++ docker/install_miniconda.sh
@@ -1,5 +1,5 @@
 # this is Ray Donnelly's custom Miniconda, built with our new gcc toolchain
-fname="$(ls -t Miniconda* | head -1)"
+fname="$(ls -t ~/Miniconda-* | head -1)"
 
 /bin/bash $fname -b -p /opt/miniconda
 rm $fname
@@ -8,4 +8,4 @@ rm $fname
 /opt/miniconda/bin/conda install --yes -c conda-canary git conda-build curl anaconda-client
 /opt/miniconda/bin/conda install --yes -c msarahan conda-concourse-ci
 /opt/miniconda/bin/conda config --add channels rdonnelly
-/opt/miniconda/bin/conda clean --tarballs --packages
+/opt/miniconda/bin/conda clean -ptiy
