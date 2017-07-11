d09cc1abaa8f3820967f1b05ad3f4dc0ec9d15c5
Michael Sarahan <msarahan@gmail.com>
2017-07-11 13:14:23 -0500

    point docker and anaconda.org at official channels

--- docker/install_miniconda.sh
+++ docker/install_miniconda.sh
@@ -5,9 +5,9 @@ rm $fname
 /opt/miniconda/bin/conda config --set show_channel_urls True
 # update --all is pulling in replacements from defaults
 # /opt/miniconda/bin/conda update --yes --all
-/opt/miniconda/bin/conda install --yes -c conda-canary git conda-build curl anaconda-client
-/opt/miniconda/bin/conda install --yes -c msarahan conda-concourse-ci
-/opt/miniconda/bin/conda config --add channels rdonnelly
+/opt/miniconda/bin/conda config --add channels c3i_test
+/opt/miniconda/bin/conda install --yes git conda-build curl anaconda-client
+/opt/miniconda/bin/conda install --yes conda-concourse-ci
 /opt/miniconda/bin/conda config --set add_pip_as_python_dependency False
 /opt/miniconda/bin/conda config --set anaconda_upload True
 /opt/miniconda/bin/conda clean -ptiy
