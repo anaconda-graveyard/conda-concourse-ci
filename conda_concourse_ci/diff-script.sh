be14ecd2bbbe5b4418905604b2e00a6e23d686ab
mandeep <mbhutani@continuum.io>
2017-07-18 14:13:46 -0500

    Fixed shellcheck issues in diff-script.sh

--- conda_concourse_ci/diff-script.sh
+++ conda_concourse_ci/diff-script.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 submodules=$(git submodule | awk '{print $2}')
-for submodule in ${submodules[*]}; do
-    revision=$(git diff $1 $submodule | fgrep "Subproject" | head -n1 | awk '{print $3}')
-    cd $submodule
-    echo "$submodule" $(git diff $revision --name-only)
-    cd ..
+for submodule in $submodules; do
+    revision=$(git diff $1 $submodule | grep -F "Subproject" | head -n1 | awk '{print $3}')
+    (
+        cd $submodule || exit
+        echo $submodule $(git diff $revision --name-only)
+    )
 done
