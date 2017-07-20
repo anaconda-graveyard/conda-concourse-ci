d22509f0c7ded4aaa2d5c19e748dce06f6e4d889
mandeep <mbhutani@continuum.io>
2017-07-20 08:49:08 -0500

    Added double quotes around expansion to prevent word splitting

--- conda_concourse_ci/new-submodule-script.sh
+++ conda_concourse_ci/new-submodule-script.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 new_submodules=$(git diff master --submodule=log | grep -F "(new submodule)" | awk '{print $2}')
 for new_submodule in $new_submodules; do
     (
-        cd $new_submodule || exit
+        cd "$new_submodule" || exit
         if find . -name 'meta.yaml' | grep -e '.' >/dev/null 2>&1; then
-            echo $new_submodule
+            echo "$new_submodule"
         fi
     )
 done
