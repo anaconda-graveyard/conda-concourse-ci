943efa35976e88a2ef0035ed4a88af9c1d93b16c
Michael Sarahan <msarahan@gmail.com>
2017-11-28 09:41:49 -0600

    add linux compilers to docker image, so that they're cached

--- docker/c3i-linux-64/install_miniconda.sh
+++ docker/c3i-linux-64/install_miniconda.sh
@@ -10,6 +10,9 @@ bash -x $fname -bfp /opt/conda
 # /opt/conda/bin/conda update --yes --all
 /opt/conda/bin/conda update --yes --quiet --all
 /opt/conda/bin/conda install --yes --quiet git conda-build curl anaconda-client
+# install compilers so that we don't need to download them for each build.  It's effectively caching.
+#    Ignore gfortran to save some space, though.  It's not used commonly enough to warrant caching.
+/opt/conda/bin/conda install --yes --quiet gcc_linux-64 gxx_linux-64
 /opt/conda/bin/pip install https://github.com/conda/conda-concourse-ci/archive/master.zip
 /opt/conda/bin/conda config --set add_pip_as_python_dependency False
 /opt/conda/bin/conda clean -ptiy
