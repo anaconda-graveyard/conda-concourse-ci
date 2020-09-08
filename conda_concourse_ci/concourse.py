ac0f8423b00a03513fa4580b99054800c4707251
Jonathan Helmus <jjhelmus@gmail.com>
2020-09-08 11:11:41 -0500

    remove used classes from concourse module

--- conda_concourse_ci/concourse.py
+++ conda_concourse_ci/concourse.py
@@ -259,54 +259,7 @@ class Pipeline:
         self.add_job('destroy_pipeline', plan)
 
 
-class _DictBacked:
-    """ Base class where attributes are stored in the _dict attribute """
-    _dict = None
-
-    def _ensure_dict_init(self):
-        if self._dict is None:
-            self.__dict__["_dict"] = {}
-
-    def __getattr__(self, attr):
-        self._ensure_dict_init()
-        if attr not in self._dict:
-            name = type(self).__name__
-            raise AttributeError(f"'{name}' object has no attribute '{attr}'")
-        return self._dict[attr]
-
-    def __setattr__(self, attr, value):
-        self._ensure_dict_init()
-        self._dict[attr] = value
-
-    def to_dict(self):
-        return self._dict
-
-
-class Resource(_DictBacked):
-    """ configuration for a concourse resource. """
-    # https://concourse-ci.org/resources.html
-
-    def __init__(self, name, type_, source, **kwargs):
-        self.name = name
-        self.type = type_
-        self.source = source
-        for attr, value in kwargs.items():
-            setattr(self, attr, value)
-
-
-class ResourceType(_DictBacked):
-    """ configuration for a concourse resource type. """
-    # https://concourse-ci.org/resource-types.html
-
-    def __init__(self, name, type_, source, **kwargs):
-        self.name = name
-        self.type = type_
-        self.source = source
-        for attr, value in kwargs.items():
-            setattr(self, attr, value)
-
-
-class Job():
+class Job:
     """ configuration for a concourse job. """
     # https://concourse-ci.org/jobs.html
 
