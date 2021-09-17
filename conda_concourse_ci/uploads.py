52ea9395748426b6bff1c2873352d93f702e21b1
Connor Martin <connormartin7@gmail.com>
2021-09-17 08:30:19 -0500

    Migrate to ECR
    Signed-off-by: Connor Martin <connormartin7@gmail.com>

--- conda_concourse_ci/uploads.py
+++ conda_concourse_ci/uploads.py
@@ -25,7 +25,7 @@ def _base_task(upload_job_name, username=None, password=None):
                 'inputs': [{'name': 'output-artifacts'}],
                 'image_resource': {
                     'type': 'docker-image',
-                    'source': {'repository': 'conda/c3i-linux-64',
+                    'source': {'repository': 'public.ecr.aws/y0o4y9o3/anaconda-pkg-build',
                                'tag': 'latest'}},
                 'platform': 'linux',
                 'run': {}
